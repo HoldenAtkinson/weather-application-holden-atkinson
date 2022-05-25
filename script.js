75cdfd5229450c52219fd173ff64e18956d23ca0
Holden A <holdenatkinson@gmail.com>
2022-05-24 18:01:04 -0600

    created more styling and began function to search bar.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $( function() {
     $( "#selectable" ).selectable();
   } );
+
+  const form = document.querySelector(".container form");
+ 
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    const inputVal = input.value;
+  });
