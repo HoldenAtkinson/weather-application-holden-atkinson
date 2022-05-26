3c350a4e9b0f2a91ef4930cbd57438374917a345
Holden A <holdenatkinson@gmail.com>
2022-05-25 18:02:36 -0600

    created 5 day forecast and attempted functionality of weather api

--- script.js
+++ script.js
@@ -2,4 +2,22 @@ $( function() {
     $( "#selectable" ).selectable();
   } );
 
-  const form = document.querySelector(".container form");
+  const button = document.querySelector('.button')
+  const inputBox = document.querySelector('.inputValue')
+  const cityName = document.querySelector('.city-name')
+  const temp = document.querySelector('.temp')
+  const wind = document.querySelector('.wind')
+  const humidity = document.querySelector('.humidity')
+  const uvIndex = document.querySelector('.uv-index')
+
+
+
+  button.addEventListener('click',function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=1e1abfef4862065d738fdd48a265724d')
+  .then(response=>response.json())
+  .then(data=>console.log(data))
+})
+
+
+
+
